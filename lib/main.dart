1190e572f99d607d470c4a793fc6058b08e97381
miya987-a <Miyaji741@gmail.com>
2026-07-03 22:22:49 +0300

    Set up Flutter app with WebView integration
    Initialize a Flutter app with a WebView to display a website.

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Global News',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const WebViewScreen(),
+    );
+  }
+}
+
+class WebViewScreen extends StatefulWidget {
+  const WebViewScreen({super.key});
+
+  @override
+  State<WebViewScreen> createState() => _WebViewScreenState();
+}
+
+class _WebViewScreenState extends State<WebViewScreen> {
+  late final WebViewController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      // নিচের লাইনে আপনার ওয়েবসাইটের আসল লিংকটি দিন
+      ..loadRequest(Uri.parse('https://আপনার-ওয়েবসাইটের-লিংক.onrender.com'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Global News'),
+        toolbarHeight: 0, // এটি ওপরের বার লুকিয়ে রাখবে যাতে পুরো স্ক্রিন জুড়ে ওয়েবসাইট দেখা যায়
+      ),
+      body: SafeArea(
+        child: WebViewWidget(controller: controller),
+      ),
+    );
+  }
+}
 
